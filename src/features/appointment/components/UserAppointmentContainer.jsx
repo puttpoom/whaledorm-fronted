cc32664b2861e56f23de50745d003aebe50f3cf6
puttpoom <poom.bm@gmail.com>
2024-03-22 06:59:50 +0700

    Style Container to fit desktop page

--- src/features/appointment/components/UserAppointmentContainer.jsx
+++ src/features/appointment/components/UserAppointmentContainer.jsx
@@ -4,11 +4,11 @@ import { useEffect } from "react";
 
 export default function UserAppointment() {
   const { userAppointments } = useAppointment();
-  console.log(userAppointments);
+  console.log(userAppointments, typeof userAppointments);
   return (
     <div className="flex flex-col gap-8 bg-[#F1F5F9] p-12 h-content overflow-auto">
-      {userAppointments.lenght < 0 ? (
-        <div>คุณยังไม่มีรายการนัดหมาย</div>
+      {!userAppointments.length > 0 ? (
+        <p className="text-lg">คุณยังไม่มีรายการนัดหมาย</p>
       ) : (
         userAppointments.map((appointment, index) => (
           <RoomCard
