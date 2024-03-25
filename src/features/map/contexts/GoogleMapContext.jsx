b84e7624b7c18d72244ed58c745bc0ac619542cb
puttpoom <poom.bm@gmail.com>
2024-03-26 04:47:59 +0700

    Delete Comments

--- src/features/map/contexts/GoogleMapContext.jsx
+++ src/features/map/contexts/GoogleMapContext.jsx
@@ -5,16 +5,12 @@ import * as mapApi from "../../../api/map";
 export const GoogleMapContext = createContext();
 
 export default function GoogleMapContextProvider({ children }) {
-  //   const { roomTarget } = useAppointment();
-  //   console.log(roomTarget.dormId, "dormId");
   const [dormLatLong, setDormLatLong] = useState(null);
-  // console.log(dormLatLong, "dormLatLong");
 
   useEffect(() => {
     const fetchLatLongDorm = async () => {
       try {
         const res = await mapApi.getLatLongDormByDormId(2);
-        // console.log(res.data, "res.data");
         setDormLatLong(res.data);
       } catch (err) {
         console.log(err);
